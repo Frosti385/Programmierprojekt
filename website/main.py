b182577a94704de6b7708fe27547593e6de3316c
Frosti385 <maxime.keuters@hs-osnabrueck.de>
2022-10-04 17:51:51 +0200

    feat: Implemented Fileupload code

--- website/main.py
+++ website/main.py
@@ -2,7 +2,8 @@
 Test implementation of a Restful API for Uploading Images 
 """
 import os
-from flask import Flask, render_template #flash, request, redirect, render_template
+from flask import Flask, render_template, flash, request, redirect
+from config import *
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
@@ -27,6 +28,46 @@ api.add_resource(HelloWorld,'/hello')
 @app.route('/upload')
 def upload_form():
     return render_template('upload.php')
+
+
+## on a POST request of data 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+
+        ### Auth
+        user_code = str(request.form.get('psw'))
+        
+        # Open database
+        #conn = sqlite3.connect(db_loc)
+        #cursor = conn.cursor()
+        #cursor.execute("PRAGMA key='%s'"%app_key)
+        
+        # Run sql query
+        #cursor.execute('select * from upload where uploadcode="%s"'%user_code)
+        #result = cursor.fetchall() 
+        
+        # close as we are done with it
+        #conn.close()
+
+        #if len(result)==0: 
+        #    # If we do not get a match, send a message
+        #    flash('Not a valid Code')
+        #    return redirect(request.url)
+        
+
+        if 'files[]' not in request.files:
+            flash('No files found, try again.')
+            return redirect(request.url)
+
+        files = request.files.getlist('files[]')
+
+        for file in files:
+            if file:              
+               file.save(os.path.join(upload_dest, file.filename))
+
+        flash('Files uploaded')
+        return redirect('/upload')
 
 
 if __name__=='__main__':
